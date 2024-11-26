fc3590f492d1b76b64cb0eed9e69b29332c1ae0c
alpertng02 <alpertunga2003@gmail.com>
2024-11-26 20:38:49 +0300

    filled msg header and added frame_id param

--- src/bno055_publisher.cpp
+++ src/bno055_publisher.cpp
@@ -16,16 +16,18 @@ using namespace std::chrono_literals;
 class MinimalPublisher : public rclcpp::Node {
 public:
     MinimalPublisher(std::string_view devDirectory, uint8_t devAddress)
-        : Node("imu_node"), imu_(devDirectory, devAddress) {
+        : Node("bno055_node"), imu_(devDirectory, devAddress) {
 
-        publisher_ = this->create_publisher<sensor_msgs::msg::Imu>("imu_msg",
-            rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_sensor_data)));
+        frameId_ = this->declare_parameter("frame_id", frameId_);
+        
+        publisher_ = this->create_publisher<sensor_msgs::msg::Imu>("imu",
+            rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_system_default)));
         timer_ = this->create_wall_timer(
             10ms, std::bind(&MinimalPublisher::timer_callback, this));
 
-        constexpr std::array accelCovariance = { 0.017, 0.017, 0.017 };
-        constexpr std::array gyroCovariance = { 0.04, 0.04, 0.04 };
-        constexpr std::array quatCovariance = { 0.0159, 0.0159, 0.0159 };
+        constexpr std::array accelCovariance = { 67.53e-06, 67.53e-06, 67.53e-06 };
+        constexpr std::array gyroCovariance = { 3.05e-06, 3.05e-06, 3.05e-06 };
+        constexpr std::array quatCovariance = { 15.9e-03, 15.9e-03, 15.9e-03 };
 
         for (size_t i = 0; i < imuMsg_.angular_velocity_covariance.size(); i++) {
             if (i % 3 == 0) {
@@ -43,7 +45,10 @@ public:
 private:
     void timer_callback() {
         try {
-            auto accel = imu_.getLinearAccelMsq();
+            imuMsg_.header.stamp = this->get_clock()->now();
+            imuMsg_.header.frame_id = frameId_;
+
+            auto accel = imu_.getAccelMsq();
             auto gyro = imu_.getGyroRps();
             auto quats = imu_.getQuaternion();
 
@@ -60,12 +65,12 @@ private:
             imuMsg_.orientation.y = quats.y;
             imuMsg_.orientation.z = quats.z;
 
-            RCLCPP_INFO(this->get_logger(), "LinearAccel: x: %3.2f,  y: %3.2f,  z: %3.2f\n", accel.x, accel.y, accel.z);
-            RCLCPP_INFO(this->get_logger(), "AngularVel : x: %3.2f,  y: %3.2f,  z: %3.2f\n", gyro.x, gyro.y, gyro.z);
-            RCLCPP_INFO(this->get_logger(), "Quaternion : w: %3.2f,  x: %3.2f,  y: %3.2f,  z: %3.2f\n\n", quats.w, quats.x, quats.y, quats.z);
+            //RCLCPP_INFO(this->get_logger(), "LinearAccel: x: %3.2f,  y: %3.2f,  z: %3.2f\n", accel.x, accel.y, accel.z);
+            //RCLCPP_INFO(this->get_logger(), "AngularVel : x: %3.2f,  y: %3.2f,  z: %3.2f\n", gyro.x, gyro.y, gyro.z);
+            //RCLCPP_INFO(this->get_logger(), "Quaternion : w: %3.2f,  x: %3.2f,  y: %3.2f,  z: %3.2f\n\n", quats.w, quats.x, quats.y, quats.z);
             publisher_->publish(imuMsg_);
         } catch (std::runtime_error& err) {
-            RCLCPP_ERROR(this->get_logger(), "Sensor connection is lost!\n");
+            RCLCPP_ERROR(this->get_logger(), "Sensor connection is lost! Trying to reconnect...\n");
             if (!imu_.reconnect()) {
                 std::this_thread::sleep_for(1s);
             }
@@ -75,6 +80,8 @@ private:
     rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr publisher_;
     sensor_msgs::msg::Imu imuMsg_ = sensor_msgs::msg::Imu();
     imu::BNO055 imu_ {};
+    
+    std::string frameId_ { "imu_link" };
 };
 
 int main(int argc, char* argv[]) {
